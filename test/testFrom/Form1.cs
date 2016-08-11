0318447a8fed9f76c7bbf1f52015bb66089aa780
fusentao <fusentao@qq.com>
2016-08-11 12:17:51 +0800

    code 1

--- test/testFrom/Form1.cs
+++ test/testFrom/Form1.cs
@@ -15,5 +15,10 @@ namespace testFrom
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
     }
 }
